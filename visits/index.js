0627e2573dfc269196a1cd663d0335449f0931ef
Mars Cheung <mars.kwong.cheung@gmail.com>
2026-08-12 18:21:32 -0700

    demonstrate use of docker-compose

--- visits/index.js
+++ visits/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const redis = require('redis')
 
 const app = express()
-const client = redis.createClient()
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+})
+client.set('visits', 0)
+
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
         res.send('Number of vists is ' + visits)
